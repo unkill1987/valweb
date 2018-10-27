d58014c8aa17bd48dfaa4edb65fcfe909ea84176
김창규 <unkill1987@naver.com>
2018-10-27 23:20:14 +0900

    프로젝트 시작

--- app/views.py
+++ app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'app/index.html',{})
+    return render(request, 'app/index.html', {})
+
 
 def charts(request):
-    return render(request, 'app/charts.html',{})
+    return render(request, 'app/charts.html', {})
+
+
+def calendar(request):
+    return render(request, 'app/calendar.html', {})
+
+
+def money(request):
+    return render(request, 'app/money.html', {})
+
+
+def people(request):
+    return render(request, 'app/people.html', {})
--- valweb/urls.py
+++ valweb/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index),
-    path('charts/', views.charts),
+    path('app/', include('app.urls')),
+
 ]
