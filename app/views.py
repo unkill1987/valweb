8cfed58c9a98faefd3d81d02cbc4273c5240ac8a
unkill <unkill1987@naver.com>
2018-11-08 17:43:23 +0900

    프로젝트 시작 기본포멧(2018.10.27 11:28) login page 추가(2018.10.28 21:28) 페이지 링크 변경(2018.10.30 13.07) 로고 색변경(2018.10.30.15:57) 폼작성(2018.10.31 11:34) 폼 파일 해시값 추출(2018.10.31 12:44) 회원가입, 로그인 추가(2018.11.1 10:57) 역할별 로그인, 회원가입(2018.11.1 13:27) 차트추가(2018.11.2. 11:46)--오류 수출자 페이지 추가 계약 공유기능 추가(2018.11.4 11:18) 대충정리

--- app/views.py
+++ app/views.py
@@ -1142,27 +1142,27 @@ def index(request):
 
 
 def charts(request):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    res1 = requests.get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD', headers=headers)
-    json_data = res1.json()
-    basePrice1 = json_data[0]['basePrice']
-    sellprice1 = json_data[0]['cashSellingPrice']
-    buyprice1 = json_data[0]['cashBuyingPrice']
-
-    res2 = requests.get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWJPY', headers=headers)
-    json_data = res2.json()
-    basePrice2 = json_data[0]['basePrice']
-    sellprice2 = json_data[0]['cashSellingPrice']
-    buyprice2 = json_data[0]['cashBuyingPrice']
-
-    res3 = requests.get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWCNY', headers=headers)
-    json_data = res3.json()
-    basePrice3 = json_data[0]['basePrice']
-    sellprice3 = json_data[0]['cashSellingPrice']
-    buyprice3 = json_data[0]['cashBuyingPrice']
-    start = datetime.datetime(2018, 10, 1)
-    end = datetime.datetime.now()
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    # res1 = requests.get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD', headers=headers)
+    # json_data = res1.json()
+    # basePrice1 = json_data[0]['basePrice']
+    # sellprice1 = json_data[0]['cashSellingPrice']
+    # buyprice1 = json_data[0]['cashBuyingPrice']
+    #
+    # res2 = requests.get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWJPY', headers=headers)
+    # json_data = res2.json()
+    # basePrice2 = json_data[0]['basePrice']
+    # sellprice2 = json_data[0]['cashSellingPrice']
+    # buyprice2 = json_data[0]['cashBuyingPrice']
+    #
+    # res3 = requests.get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWCNY', headers=headers)
+    # json_data = res3.json()
+    # basePrice3 = json_data[0]['basePrice']
+    # sellprice3 = json_data[0]['cashSellingPrice']
+    # buyprice3 = json_data[0]['cashBuyingPrice']
+    # start = datetime.datetime(2018, 10, 1)
+    # end = datetime.datetime.now()
 
     try:
 
@@ -1181,9 +1181,10 @@ def charts(request):
         else:
             templates = 'app/login.html'
 
-        return render(request, templates, {'basePrice1': basePrice1,'sellprice1':sellprice1,'buyprice1':buyprice1,
-                                           'basePrice2':basePrice2,'sellprice2':sellprice2,'buyprice2':buyprice2,
-                                           'basePrice3':basePrice3,'sellprice3':sellprice3,'buyprice3':buyprice3})
+        # return render(request, templates, {'basePrice1': basePrice1,'sellprice1':sellprice1,'buyprice1':buyprice1,
+        #                                    'basePrice2':basePrice2,'sellprice2':sellprice2,'buyprice2':buyprice2,
+        #                                    'basePrice3':basePrice3,'sellprice3':sellprice3,'buyprice3':buyprice3})
+        return render(request, templates, {})
     except:
         return redirect('login')
 
